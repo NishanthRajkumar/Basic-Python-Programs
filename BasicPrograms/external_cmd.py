26f99c43ebf43877e5f64d79de79014d105efcce
Nishanth <rajkumarnishanth@gmail.com>
2022-03-28 17:44:06 +0530

    feat: execute system commands in python program

--- BasicPrograms/external_cmd.py
+++ BasicPrograms/external_cmd.py
@@ -5,16 +5,24 @@
     @Last Modified time: 28-03-2022 17:20:00
     @Title: execute external command
 '''
+import logging
+import os
+import sys
 
-def external_cmd():
+def external_cmd(command):
     """
         Description:
             calls an external command in Python.
         
         Parameter:
-            None
+            Accepts command string
         
         Return:
             None
     """
-    
+    os.system(command)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(handlers=[logging.FileHandler(r"basic_programs.log"), logging.StreamHandler(sys.stdout)], level=logging.INFO)
+    external_cmd(r'calc')
