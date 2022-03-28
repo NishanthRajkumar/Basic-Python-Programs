29de70d4bb9894b3eff751cb35b6a0ca5cd8acbe
Nishanth <rajkumarnishanth@gmail.com>
2022-03-28 17:26:58 +0530

    doc: added doc string

---
+++ BasicPrograms/external_cmd.py
@@ -0,0 +1,20 @@
+'''
+    @Author: Nishanth
+    @Date: 28-03-2022 17:00:00
+    @Last Modified by: Nishanth
+    @Last Modified time: 28-03-2022 17:20:00
+    @Title: execute external command
+'''
+
+def external_cmd():
+    """
+        Description:
+            calls an external command in Python.
+        
+        Parameter:
+            None
+        
+        Return:
+            None
+    """
+    
