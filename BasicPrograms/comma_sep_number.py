a5d3739d9d27192a42a365ec7344f81761698cc3
Nishanth <rajkumarnishanth@gmail.com>
2022-03-28 22:00:45 +0530

    docs: add informative doc strings

--- BasicPrograms/comma_sep_number.py
+++ BasicPrograms/comma_sep_number.py
@@ -1,3 +1,10 @@
+'''
+    @Author: Nishanth
+    @Date: 28-03-2022 17:00:00
+    @Last Modified by: Nishanth
+    @Last Modified time: 28-03-2022 17:00:00
+    @Title: Generate a list and a tuple using comma separated numbers
+'''
 import logging
 import sys
 
