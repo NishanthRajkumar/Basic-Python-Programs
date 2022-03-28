443c64bb364fc8c60738e5a23afe466ee7dcc849
Nishanth <rajkumarnishanth@gmail.com>
2022-03-28 16:58:24 +0530

    feat: convert comma separated input as list

---
+++ BasicPrograms/comma_sep_number.py
@@ -0,0 +1,25 @@
+import logging
+import sys
+
+def comma_sep_numbers():
+        """
+            Description:
+                accepts a sequence of comma-separated numbers from user.
+                Generate a list and a tuple with those numbers
+                
+            Parameters:
+                None
+            
+            Return:
+                None
+        """
+        user_input = input("Enter multiple nos seperated by ',': ")
+        number_list = list(map(int, user_input.split(',')))
+        number_tuple = tuple(number_list)
+        logging.info(f"Number list: {number_list}")
+        logging.info(f"Number tuple: {number_tuple}")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(handlers=[logging.FileHandler(r"basic_programs.log"), logging.StreamHandler(sys.stdout)], level=logging.INFO)
+    comma_sep_numbers()
